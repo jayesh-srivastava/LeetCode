d97ea45d045f8011c692d8947e6623809e56fbd4
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-23 02:04:43 +0530

    Update Solution2.java

--- 30-day-leetcoding-challenge/SingleNumber/Solution2.java
+++ 30-day-leetcoding-challenge/SingleNumber/Solution2.java
@@ -1,1 +1,21 @@
-
+class Solution
+{
+    public int singleNumber(int[] nums)
+    {
+        int l=nums.length;
+        Arrays.sort(nums);
+        int x=0;
+        while(x<l-1)
+        {
+            if(nums[x]==nums[x+1])
+            {
+                x+=2;
+            }
+            else
+            {
+                return nums[x];
+            }
+        }
+        return nums[x];
+    }
+}
