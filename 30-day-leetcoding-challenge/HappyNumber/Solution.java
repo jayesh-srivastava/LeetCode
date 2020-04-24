bccfe036ce6af96c66860cf86029e258d7b813f6
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-24 22:38:19 +0530

    Update Solution.java

--- 30-day-leetcoding-challenge/HappyNumber/Solution.java
+++ 30-day-leetcoding-challenge/HappyNumber/Solution.java
@@ -1,1 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum=0;
+        Set<Integer> set=new HashSet<>();
+        do {
+            sum=0;
+            while(n!=0) {
+                int d=n%10;
+                n=n/10;
+                sum+=d*d;
+            }
+            n=sum;
+        }while(n>1 && set.add(n));
+        if(n>1)
+            return false;
+        else
+            return true;
+    }
 
