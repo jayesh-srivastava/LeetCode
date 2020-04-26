99a24342e096c122d9c474af6147d3bcf7410d16
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-26 23:28:28 +0530

    Update Solution.java

--- Median of two sorted arrays/Solution.java
+++ Median of two sorted arrays/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        List<Integer> l=new ArrayList<Integer>();
+        List<Integer> list=new ArrayList<Integer>();
         double f;
         for(int i=0;i<nums1.length;i++) 
-            l.add(nums1[i]);
+            list.add(nums1[i]);
         for(int j=0;j<nums2.length;j++)
-               l.add(nums2[j]);
-        Collections.sort(l);
-        int k=l.size();
+               list.add(nums2[j]);
+        Collections.sort(list);
+        int k=list.size();
         if(k%2==0)
-           f=(double)((l.get(k/2-1))+(l.get(k/2)))/2;
+           f=(double)((list.get(k/2-1))+(list.get(k/2)))/2;
         else 
-            f = l.get(((k + 1) / 2)-1);
+            f = list.get(((k + 1) / 2)-1);
         return f;
     }
 }
