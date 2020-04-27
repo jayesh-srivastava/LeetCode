529f9e6b00955c3c703e1447873f047a0827a4e4
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-27 23:29:09 +0530

    Update Solution.java

--- 30-day-leetcoding-challenge/MaximumSubarray/Solution.java
+++ 30-day-leetcoding-challenge/MaximumSubarray/Solution.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int maxSubArray(int[] a) {
+        int size = a.length; 
+        int maxsofar = Integer.MIN_VALUE, maxendinghere = 0; 
+  
+        for (int x = 0; x < size; x++) 
+        { 
+            maxendinghere = maxendinghere + a[x]; 
+            if (maxsofar < maxendinghere) 
+                maxsofar = maxendinghere; 
+            if (maxendinghere < 0) 
+                maxendinghere = 0; 
+        } 
+        return maxsofar;
+        
+    }
+}
