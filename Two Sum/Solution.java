d25ef0c920d3810c4fb739a167190d3e79e447fd
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-29 23:45:08 +0530

    Update Solution.java

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -2,8 +2,8 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target)
     {
-    Map<Integer, Integer> map = new HashMap<>();
-    int arr[]=new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        int arr[]=new int[2];
     for (int i = 0; i < nums.length; i++) {
         map.put(nums[i], i);
     }
