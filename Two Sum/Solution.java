d06758ee21e7fdde69fb0e39920a09aaf6c05751
Jayesh Srivastava <jayesh0200@gmail.com>
2020-04-29 23:46:11 +0530

    Update Solution.java

--- Two Sum/Solution.java
+++ Two Sum/Solution.java
@@ -11,11 +11,12 @@ class Solution
         for (int i = 0; i < nums.length; i++)
         {
             int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-            arr[0]=i;
-            arr[1]=map.get(complement);
+            if (map.containsKey(complement) && map.get(complement) != i)
+            {
+                arr[0]=i;
+                arr[1]=map.get(complement);
+            }
         }
-    }
-    return new int[] {arr[1],arr[0]};
-} 
+        return new int[] {arr[1],arr[0]};
+    } 
 }
