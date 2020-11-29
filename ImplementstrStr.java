e90c4e2acc92c5d7f61c9383203391ff009b9228
Jayesh Srivastava <jayesh0200@gmail.com>
2020-11-29 16:23:33 +0530

    Update ImplementstrStr.java

--- ImplementstrStr.java
+++ ImplementstrStr.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.length()==0)
+            return 0;
+        int flag=-1;
+        int length=needle.length();
+        for(int x=0;x<=(haystack.length()-needle.length());x++) {
+            String sub=haystack.substring(x,length);
+            if(sub.equalsIgnoreCase(needle)) {
+                flag=x;
+                x=haystack.length();
+            }
+            length++;
+        }
+        return flag;
+    }
+}
