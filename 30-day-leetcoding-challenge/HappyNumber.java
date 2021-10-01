f70001e3ec1033e3232faffb3f7545c1dd213b87
Pradhumna Banerji <60711601+pradhumna22@users.noreply.github.com>
2021-10-01 10:01:25 +0530

    Update HappyNumber.java
    For the hacktober fest

--- 30-day-leetcoding-challenge/HappyNumber.java
+++ 30-day-leetcoding-challenge/HappyNumber.java
@@ -17,4 +17,24 @@ class Solution {
             return true;
     }
 }
+or
+ class Solution {
+ public boolean isHappy(int n) {
+        int sl = n, fa = n;
+        do {
+            sl = calcu(sl);
+            fa = calcu(calcu(fa));
+        } while (sl != fa);
+        return sl == 1;
+    }
 
+    private int calcu(int n) {
+        int rem = 0;
+        while (n > 0) {
+            int d = n % 10;
+            rem += d * d;
+            n /= 10;
+        }
+        return rem;
+    }
+}
